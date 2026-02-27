d3f6eafa08f597d723d4950fdf1fe9912ea60af6
Max_dev <maxime2008.lucas@gmail.com>
2026-02-27 23:10:51 +0100

    Setting the server and html page

--- Server_side/Main_AI_text_return.py
+++ Server_side/Main_AI_text_return.py
@@ -1,25 +1,51 @@
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
-asked_question = input("What is your question? ")
+
+app = Flask(__name__)
+CORS(app)
+
+
+#Token management
 TokenAvailable = 100
 TokenUsed = 0
+
+# Load the model and tokenizer
 model_id = "Qwen/Qwen3-0.6B"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float32)
-messages = [
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": asked_question}
-]
-Input_text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-TokenizeInput = tokenizer(Input_text, return_tensors="pt")
-with torch.no_grad():
-    outputs = model.generate(
+
+
+@app.route('/get_response', methods=['POST'])
+def response_send():
+    #question input
+    asked_question = request.json["message"]
+    #Create the message
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": asked_question}
+    ]
+
+    #Create the input for the model
+    Input_text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    TokenizeInput = tokenizer(Input_text, return_tensors="pt")
+
+    #Generate the response
+    with torch.no_grad():
+        outputs = model.generate(
         **TokenizeInput,
         max_new_tokens=200,
         temperature=0.7,
         do_sample=True
     )
-response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(response)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(response)
+    return jsonify({"reponse": response}), print("hello world")
+
+    #Launch Server
+if __name__ == "__main__":
+    app.run(port=5000, debug=False)
+    
+        
